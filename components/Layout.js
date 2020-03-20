696f51d95137fd09df0bc92f889b8debfee0310b
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-03-20 00:07:15 -0400

    Imports the 'Head' module from the 'next/head' package into the <Layout /> component

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,11 @@
+import Head from 'next/head';
 import Navbar from './Navbar';
 
 const Layout = props => (
 	<div>
+		<Head>
+			<title>Tico's Bitcoin Prices App</title>
+		</Head>
 		<Navbar />
 		{props.children}
 	</div>
