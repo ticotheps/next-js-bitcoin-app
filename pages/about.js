d74e3b95e83926b022f4fe7d5af49ceb1f73af7b
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-03-19 23:50:01 -0400

    Refactors the 'about.js' file to have more semantic HTML tags & to update content

--- pages/about.js
+++ pages/about.js
@@ -1,1 +1,8 @@
-export default () => <div>About Tico's Next.js Bitcoin App!</div>;
+const About = () => (
+	<div>
+		<h1>About Tico's Bitcoin Prices App</h1>
+		<p>An application used to view Bitcoin prices!</p>
+	</div>
+);
+
+export default About;
