9e182157d8a38005c0441015e8afa04f74f2058a
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-03-19 23:52:28 -0400

    Imports the 'Link' module from the 'next/link' package into both, the 'index.js' and the 'about.js' files, to allow for navigation from page to page

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,15 @@
+import Link from 'next/link';
+
 const About = () => (
 	<div>
+		<ul>
+			<li>
+				<Link href='/'>Home</Link>
+			</li>
+			<li>
+				<Link href='/about'>About</Link>
+			</li>
+		</ul>
 		<h1>About Tico's Bitcoin Prices App</h1>
 		<p>An application used to view Bitcoin prices!</p>
 	</div>
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
+import Link from 'next/link';
+
 const Index = () => (
 	<div>
+		<ul>
+			<li>
+				<Link href='/'>Home</Link>
+			</li>
+			<li>
+				<Link href='/about'>About</Link>
+			</li>
+		</ul>
 		<h1>Welcome to Tico's Bitcoin Prices App!</h1>
 	</div>
 );
