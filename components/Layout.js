ce81011227ca4e5425ba6a1aac93c30c06b46d64
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-03-20 00:28:52 -0400

    Adds a custom bootstrap CDN in a <link> tag to the <Head> tag in the 'Layout.js' file

--- components/Layout.js
+++ components/Layout.js
@@ -5,6 +5,10 @@ const Layout = props => (
 	<div>
 		<Head>
 			<title>Tico's Bitcoin Prices App</title>
+			<link
+				rel='stylesheet'
+				href='https://bootswatch.com/4/lux/bootstrap.min.css'
+			></link>
 		</Head>
 		<Navbar />
 		{props.children}
