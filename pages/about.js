fa1a886fc0f90b1e6a36a11ce8232cfafa849822
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-03-20 00:05:04 -0400

    Removes the import for 'Navbar' component from the 'about.js' file; wraps the parent <div> of the 'about.js' page with a <Layout /> tag to allow for rendering of <Navbar />

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,12 @@
-import Navbar from '../components/Navbar';
+import Layout from '../components/Layout';
 
 const About = () => (
-	<div>
-		<Navbar />
-		<h1>About Tico's Bitcoin Prices App</h1>
-		<p>An application used to view Bitcoin prices!</p>
-	</div>
+	<Layout>
+		<div>
+			<h1>About Tico's Bitcoin Prices App</h1>
+			<p>An application used to view Bitcoin prices!</p>
+		</div>
+	</Layout>
 );
 
 export default About;
