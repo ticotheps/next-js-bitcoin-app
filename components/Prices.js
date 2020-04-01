52230f37bb215d593f6dadc52d773db333fd1f0a
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-03-31 23:33:20 -0400

    Creates a new 'list' variable in the 'render()' method that is set to an empty string; creates an series of 'if/else' blocks that determine whether 'this.state.currency' is equal to: USD, GBP, or EUR and executes the block's code based on if the condition was fulfilled

--- components/Prices.js
+++ components/Prices.js
@@ -4,17 +4,25 @@ class Prices extends React.Component {
 	};
 
 	render() {
+		let list = '';
+
+		if (this.state.currency === 'USD') {
+			list = (
+				<li className='list-group-item'>
+					Bitcoin Rate for {this.props.bpi.USD.description}:{' '}
+					<span className='badge badge-primary'>
+						{this.props.bpi.USD.code}
+					</span>
+					<strong> {this.props.bpi.USD.rate}</strong>
+				</li>
+			);
+		} else if (this.state.currency === 'GBP') {
+		} else if (this.state.currency === 'EUR') {
+		}
+
 		return (
 			<div>
-				<ul className='list-group'>
-					<li className='list-group-item'>
-						Bitcoin Rate for {this.props.bpi.USD.description}:{' '}
-						<span className='badge badge-primary'>
-							{this.props.bpi.USD.code}
-						</span>
-						<strong> {this.props.bpi.USD.rate}</strong>
-					</li>
-				</ul>
+				<ul className='list-group'>{list}</ul>
 			</div>
 		);
 	}
