3acea624e4ba06c1535ddbdb361fa0ca462a7b65
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-03-31 23:09:57 -0400

    Creates a function for the 'getInitialProps' Next.js lifecycle method in index.js

--- pages/index.js
+++ pages/index.js
@@ -9,4 +9,18 @@ const Index = () => (
 	</Layout>
 );
 
+Index.getInitialProps = async function() {
+	// The result uses the 'fetch()' method to hit the coindesk API endpoint
+	const res = await fetch('https://api.coindesk.com/v1/bpi/currentprice.json');
+
+	// Since 'fetch()' returns a promise, we need to map it to json, so we will
+	// use another 'await'
+	const data = await res.json();
+
+	return {
+		// 'bpi' = Bitcoin Price Index
+		bpi: data.bpi
+	}
+};
+
 export default Index;
