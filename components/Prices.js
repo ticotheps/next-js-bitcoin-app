8c3a952bbdcb8cdd81b4b9abeec7b3d0a492a07f
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-03-31 23:27:19 -0400

    Pulls in data from coindesk API endpoint by using 'this.props' in Prices.js

--- components/Prices.js
+++ components/Prices.js
@@ -4,7 +4,19 @@ class Prices extends React.Component {
 	};
 
 	render() {
-		return <div>Prices</div>;
+		return (
+			<div>
+				<ul className='list-group'>
+					<li className='list-group-item'>
+						Bitcoin Rate for {this.props.bpi.USD.description}:{' '}
+						<span className='badge badge-primary'>
+							{this.props.bpi.USD.code}
+						</span>
+						<strong> {this.props.bpi.USD.rate}</strong>
+					</li>
+				</ul>
+			</div>
+		);
 	}
 }
 
