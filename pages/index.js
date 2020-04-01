1c98ccf15b0036d7d007c7e2e4ba321333cb51e4
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-03-31 23:28:33 -0400

    Adds a <p> element to the index.js page, under the <h1> element

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,11 @@ import Fetch from 'isomorphic-unfetch';
 import Layout from '../components/Layout';
 import Prices from '../components/Prices';
 
-const Index = (props) => (
+const Index = props => (
 	<Layout>
 		<div>
 			<h1>Welcome to Tico's Crypto Store!</h1>
+			<p>Check current Bitcoin rate</p>
 			<Prices bpi={props.bpi} />
 		</div>
 	</Layout>
@@ -13,7 +14,9 @@ const Index = (props) => (
 
 Index.getInitialProps = async function() {
 	// The result uses the 'fetch()' method to hit the coindesk API endpoint
-	const res = await fetch('https://api.coindesk.com/v1/bpi/currentprice.json');
+	const res = await fetch(
+		'https://api.coindesk.com/v1/bpi/currentprice.json'
+	);
 
 	// Since 'fetch()' returns a promise, we need to map it to json, so we will
 	// use another 'await'
